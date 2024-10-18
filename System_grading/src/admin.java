9352d704cfbe0a8db025e56df4e91f340876f9e9
RAYTECHsudo <raymond.kasozi@studmc.kiu.ac.ug>
2024-10-18 14:51:30 -0700

    methods and variable

--- System_grading/src/admin.java
+++ System_grading/src/admin.java
@@ -1,2 +1,33 @@
 public class admin {
+    private int AdminId, Phone;
+    private String Dept,emailId;
+    public admin(String name){
+        System.out.print("ENTER ADMIN's NAME:" + name);
+    }
+   public void setAdminId(int id){
+    AdminId = id;
+} 
+   public int getAdminId(){
+       return AdminId;
+   }
+   public void setPhone(int number){
+    Phone = number;
+} 
+   public int getPhone(){
+       return Phone;
+   }
+   public void setDept(String name){
+    Dept = name;
+} 
+   public String getDept(){
+       return Dept;
+   }
+   public void setemailId(String id){
+    emailId = id;
+     
+} 
+   public String getemailId(){
+       return emailId;
+   }
+   
 }
