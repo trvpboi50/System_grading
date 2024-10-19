6831b8d488f28d1896f1adf3345c674cdc7185cb
trvpboi50 <denis.kasolo@studmc.kiu.ac.ug>
2024-10-19 13:04:19 +0300

    added variables

--- System_grading/src/student.java
+++ System_grading/src/student.java
@@ -1,2 +1,13 @@
 public class student {
+    private int id;
+    private String Reg_Id;
+    private String name;
+    private String course;
+    private String email;
+    private String phone;
+    private String department;
+
+
+
+
 }
