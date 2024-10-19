0c8fa86229bcc78b3fe8559bb962aae56ae09a74
PuttoTyron <keith.hiisa@studmc.kiu.ac.ug>
2024-10-19 05:11:08 -0700

    Update course.java

--- System_grading/src/course.java
+++ System_grading/src/course.java
@@ -1,2 +1,18 @@
-public class course {
-}
+public class Course {
+    public String courseCode;
+    //public String courseName;
+    public Course(){
+        System.out.println("Courses: ");
+    }
+    public void setCourseCode(String code){
+        courseCode = code;
+    }
+    public String getCourseCode(){
+        return courseCode;
+    }
+    /*public void setCourseName(String name){
+        courseName = name;
+    }
+    public String getCourseName(){
+        return courseName;
+    }*/
