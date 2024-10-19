6d4313c85e0cec3403a6785e1e389b573f891e41
trvpboi50 <denis.kasolo@studmc.kiu.ac.ug>
2024-10-19 16:01:25 +0300

    edited

--- System_grading/src/course.java
+++ System_grading/src/course.java
@@ -1,18 +1,45 @@
-public class Course {
-    public String courseCode;
-    //public String courseName;
-    public Course(){
-        System.out.println("Courses: ");
+import java.util.Scanner;
+
+public class course {
+    private String courseCode;
+    private String course_title;
+
+    //constructors
+    public course (){}
+    public course(String courseCode,String course_title) {
+        this.course_title=course_title;
+        this.courseCode=courseCode;
     }
-    public void setCourseCode(String code){
-        courseCode = code;
+    //setter methods
+    public void setCourseCode(String code) {
+        this.courseCode =courseCode;
     }
-    public String getCourseCode(){
+    public void setCourse_title(String course_title){
+        this.course_title=course_title;
+    }
+    //getter methods
+    public String getCourseCode() {
         return courseCode;
     }
-    /*public void setCourseName(String name){
+    public String getCourse_title(){
+        return course_title;
+    }
+    //diplay methods
+    public void dislayMethods(){
+        System.out.println("Course ID "+ courseCode);
+        System.out.println("Course title ;"+course_title);
+    }
+    //SetDetails
+    public void setDetails(Scanner input){
+        System.out.println("enter course_code :");
+        this.courseCode=input.nextLine();
+        System.out.println("enter course_Title :");
+        this.course_title=input.nextLine();
+    }
+    /* public void setCourseName(String name){
         courseName = name;
     }
     public String getCourseName(){
         return courseName;
     }*/
+}
