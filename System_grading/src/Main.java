9aafefd020607e817d994b2a0518a66afbb25c71
trvpboi50 <denis.kasolo@studmc.kiu.ac.ug>
2024-10-19 16:01:25 +0300

    Merge branch 'main' of https://github.com/trvpboi50/System_grading

--- System_grading/src/Main.java
+++ System_grading/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+       course Course=new course();
+        student Student=new student();
+        Student.displaySetdails();
+        Course.getCourse_title();
+
+
     }
 }
--- System_grading/src/admin.java
+++ System_grading/src/admin.java
@@ -1,3 +1,45 @@
-public class admin {
+/*public class admin {
+    private int AdminId;
+    private int Phone;
+    private String Dept;
+    private String emailId;
+    private String password;
+    private String name;
 
-}
+   public admin(){
+
+        System.out.print("ENTER ADMIN's NAME:" + name);
+    }
+    public void setAdminId(int id){
+        AdminId = id;
+    }
+    public int getAdminId(){
+        return AdminId;
+    }
+    public void setPhone(int number){
+        Phone = number;
+    }
+    public void setpassword(String security){
+        password = security;
+    }
+    public String getpassword(){
+        return password;
+    }
+    public int getPhone(){
+        return Phone;
+    }
+    public void setDept(String name){
+        Dept = name;
+    }
+    public String getDept(){
+        return Dept;
+    }
+    public void setemailId(String id){
+        emailId = id;
+
+    }
+    public String getemailId(){
+        return emailId;
+    }
+
+}*/
